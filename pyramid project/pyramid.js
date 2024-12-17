8ebca1552830c0cd9485a8c97644a61fb68a05ad
atismall <atismall128@gmail.com>
2024-12-17 12:12:32 +0100

    update on the pyramid project

--- pyramid project/pyramid.js
+++ pyramid project/pyramid.js
@@ -1,10 +1,24 @@
-let star = "#";
-let result = [];
+let hashtag = "#";
+let rows = [];
 let count = 8;
-let pyramid;
+console.log(rows.length);
 
-for(let i = 0; i < count; i++){
-  pyramid = star.mu
+function rowCharacter(rowNumber, rowCount){
+    return " ".repeat(rowCount - rowNumber) + hashtag.repeat(2*rowNumber-1) + " ".repeat(rowCount - rowNumber);
 }
 
-result += pyramid + "/n";
+// for(let i = 1; i <= count; i++){
+//     rows.push(rowCharacter(i, count));
+// }
+
+while (rows.length + 1 < count){
+    rows.push(rowCharacter(rows.length + 1, count));
+    console.log(rows.length)
+}
+let result = "";
+for(const row of rows){
+    console.log(row)
+    // result = result + row + "\n";
+}
+
+// console.log(result);
---
+++ pyramid project/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+while (rows.length < count){
+    rows.push(rowCharacter(rows.length + 1, count));
+}
