222acbfb28f1d489cb94b91eba7487103f066044
atismall <atismall128@gmail.com>
2024-12-19 11:59:56 +0100

    updates

--- Calorie meter Counter/script.js
+++ Calorie meter Counter/script.js
@@ -1,1 +1,6 @@
-const calorieCounter = document.getElementById("#calorie_counter");
+const calorieCounter = document.getElementById("calorie_counter");
+const budgetNumber = document.getElementById("budget");
+const entryDropdown = document.getElementById("entry_dropdown");
+const addEntry = document.getElementById("add_entry");
+const clear = document.getElementById("clear");
+const output = document.getElementById("output");
