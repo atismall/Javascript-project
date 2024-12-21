532965aae6fb71fdcd3b2db3f1e5c58b37248fc9
atismall <atismall128@gmail.com>
2024-12-21 22:11:10 +0100

    Update script.js

--- Calorie meter Counter/script.js
+++ Calorie meter Counter/script.js
@@ -11,19 +11,38 @@ function isCleanString(str) {
     return str.replace(regex, "");
 }
 
-function isValidInput(str) {
+function isInValidInput(str) {
     const regex = /\d+e\d+/i;
     return str.match(regex);
 }
 
 function addEntry(){
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input_container`);
-    const entryNumber = targetInputContainer.querySelectorAll(`input[type="text"]`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
     const HTMLString = `
     <label for="${entryDropdown.value}_${entryNumber}_name"> Entry ${entryNumber} Name</label>
     <input type="text" placeholder="Name" id="${entryDropdown.value}_${entryNumber}_name" />
     <label for="${entryDropdown.value}_${entryNumber}_calories"> Entry ${entryNumber} Calories</label>
     <input type="number" placeholder="Calories" id="${entryDropdown.value}_${entryNumber}_calories" />
     `
-    targetInputContainer.innerHTML += HTMLString;
+    targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
+
+function getCaloriesFromInput(list) {
+    let calories = 0;
+
+    for (const item of list) {
+        const currVal = isCleanString(item.value);
+        const invalidInpuMatch = isInValidInput(currVal)
+
+        if (invalidInpuMatch) {
+            alert(`Invalid Input: ${invalidInpuMatch[0]}`);
+            isError = true;
+            return null;
+        }
+        calories += Number(currVal);
+    }
+    return calories;
+}
+
+addEntryButton.addEventListener("click", addEntry);
