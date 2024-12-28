4801150378e55c324032e92ca9e8004b794d3bbf
atismall <atismall128@gmail.com>
2024-12-28 23:43:05 +0100

    Update testing.js

--- testing.js
+++ testing.js
@@ -22,9 +22,10 @@ const myArray = [];
 const arr = [
     [1, 2], [3, 4], [5, 6]
   ];
-  
+  let product = 1;
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr[i].length; j++) {
-      console.log(indexOf(arr[i]) + ", " + indexOf(arr[j]));
+      product *= arr[i][j];
     }
   }
+  console.log(product);
