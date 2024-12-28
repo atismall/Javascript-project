023c10408e31f7be337626e919066aaf59582d63
atismall <atismall128@gmail.com>
2024-12-28 22:01:40 +0100

    Update testing.js

--- testing.js
+++ testing.js
@@ -5,7 +5,26 @@ const myArray = [];
 //   myArray.unshift(i);
 //   i++;
 // }
-for (let i = 0; i < 6; i++) {
-    myArray.push(i)
+// for (let i = 0; i < 6; i++) {
+//     myArray.push(i)
+//   }
+
+// for (let i = 1; i < 10; i += 2 ) {
+//     myArray.push(i);
+//   }
+// const myArr = [2, 3, 4, 5, 6];
+// let total = 0;
+// for (let i = 0; i < myArr.length; i++){
+//     total += myArr[i]
+//   }
+// console.log(total)
+
+const arr = [
+    [1, 2], [3, 4], [5, 6]
+  ];
+  
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      console.log(indexOf(arr[i]) + ", " + indexOf(arr[j]));
+    }
   }
-console.log(myArray)
