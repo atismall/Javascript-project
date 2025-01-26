367593c4de556ebdca8337d47529c8e725900d70
atismall <atismall128@gmail.com>
2025-01-26 23:31:05 +0100

    Update script.js

--- Music Player/script.js
+++ Music Player/script.js
@@ -145,6 +145,8 @@ playButton.addEventListener("click", () => {
       }
 });
 
+pauseButton.addEventListener("click",  pauseSong);
+
 const sortSongs = () => {
     userData?.songs.sort((a,b) => {
       if (a.title < b.title) {
