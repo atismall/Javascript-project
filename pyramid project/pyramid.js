aa7ca8b2e395a694218ae8ebcccd66a48674de96
atismall <atismall128@gmail.com>
2025-01-30 08:05:01 +0100

    Update pyramid.js

--- pyramid project/pyramid.js
+++ pyramid project/pyramid.js
@@ -2,15 +2,22 @@
 const symbol = "#";
 const count = 8;
 const rows = [];
+let inverted = true;
 
 
-// for (let i = 1; i <= count; i++) {
-//     rows.unshift(pyramidGenerator(i, count))
+for (let i = 1; i <= count; i++) {
+    if(inverted) {
+        rows.push(pyramidGenerator(i, count))
+    } else {
+        rows.unshift(pyramidGenerator(i, count))
+    }
+}
+
+// while(rows.length < count) {
+//     rows.push(pyramidGenerator(rows.length + 1, count))
 // }
 
-while(rows.length < count) {
-    rows.push(pyramidGenerator(rows.length + 1, count))
-}
+
 
 function pyramidGenerator (rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + symbol.repeat(2*rowNumber - 1) + " ".repeat(rowCount - rowNumber)
